9f1965e3ffc4d568258bd645a1710d89f0483685
swx <swx@swxdeMacBook-Pro.local>
2016-09-13 15:13:18 +0800

    init demo

---
+++ StudyDemo/app/src/main/java/com/swx/studydemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.swx.studydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_confirm)
+    TextView tvConfirm;
+    @BindView(R.id.tv_cancel)
+    TextView tvCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        init();
+    }
+
+    private void init() {
+        tvConfirm.setOnClickListener(onClickListener);
+        tvCancel.setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.tv_confirm: {
+                    Toast.makeText(v.getContext(), "confirm", Toast.LENGTH_SHORT).show();
+                }
+                break;
+                case R.id.tv_cancel: {
+                    Toast.makeText(v.getContext(), "cancel", Toast.LENGTH_SHORT).show();
+                }
+                break;
+                default:
+                    break;
+            }
+        }
+    };
+}
